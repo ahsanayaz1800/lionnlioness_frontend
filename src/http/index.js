07cc379ecfc778d5fef12f31e5167017d6409461
ahsanayaz1800 <143333217+ahsanayaz1800@users.noreply.github.com>
2024-10-08 12:00:03 +0500

    Update index.js

--- src/http/index.js
+++ src/http/index.js
@@ -6,4 +6,4 @@ const api = axios.create({
 })
 
 
-export const googleAuthLogin = (data) => axios.post('http://localhost:8080/users/google_signin',data);
+export const googleAuthLogin = (data) => axios.post('https://lionnlioness-backend-djb6bmdmx-lionnlioness-projects.vercel.app/users/google_signin',data);
