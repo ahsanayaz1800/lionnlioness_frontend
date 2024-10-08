e47b1c0ee88b978ad8646af6c0efe8f9c497ee74
ahsanayaz1800 <143333217+ahsanayaz1800@users.noreply.github.com>
2024-10-08 17:54:31 +0500

    Update Register.js

--- src/containers/Register.js
+++ src/containers/Register.js
@@ -307,7 +307,7 @@ errorPosition = error => {
 
 fetchLocationDetails = (latitude, longitude) => {
   // Example using IP-API to get location details
-  fetch(`https://ip-api.com/json`)
+  fetch(`http://ip-api.com/json`)
     .then(response => response.json())
     .then(data => {
       const locationDetails = {
