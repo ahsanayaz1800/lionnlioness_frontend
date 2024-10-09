92ba20109c3cf7ec2d577cd2f8132148ad8f606d
ahsanayaz1800 <143333217+ahsanayaz1800@users.noreply.github.com>
2024-10-09 13:47:50 +0500

    Update index.js

--- src/http/index.js
+++ src/http/index.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 import 'dotenv/config'
-const api = axios.create({
-  baseURL:`${process.env.REACT_APP_BASE_URL}`,
-  
-})
+const baseURL = process.env.REACT_APP_BASE_URL;
 
 
-export const googleAuthLogin = (data) => axios.post('https://lionnlioness-backend-git-master-lionnlioness-projects.vercel.app/users/google_signin',data);
+
+export const googleAuthLogin = (data) => axios.post(`${baseURL}/users/google_signin`,data);
  
