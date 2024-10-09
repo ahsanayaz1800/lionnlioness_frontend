cb8e605bdbc41142f2232ae4f1622de4539893a7
ahsanayaz1800 <143333217+ahsanayaz1800@users.noreply.github.com>
2024-10-09 13:34:15 +0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -25,7 +25,7 @@ class Footer extends Component {
           </div>
           <div className="footer-copyright">
             <div className="container">
-            © 2019 Copyright Matcha
+            © 2019 Copyright Lion n Lioness
             <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
             </div>
           </div>
